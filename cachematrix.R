0edf4e1160cbb4af2fa75d63bb5ee01a9e4ede25
Tyler <tyler.j.bohnsack@gmail.com>
2018-06-26 15:58:57 -0400

    cache matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## programs that will compute inverse of matrix, cache this, 
+## and will retrieve cache value if already computed upon new runs
 
-## Write a short comment describing this function
+## makes list of run functions
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {                        #   this 
+                       x <<- y                  #   part
+                       m <<- NULL               #   defines
+    }                                           #  
+                                                #   the
+    get <- function() x                         #  
+    setSolve <- function(solve) m <<- solve     #   run
+    getSolve <- function() m                    #   functions
+    
+    list(set = set, get = get,                  #   create list 
+         setSolve = setSolve,                   #   of run
+         getSolve = getSolve)                   #   functions
+    }
 
+## gets and returns cache if computed else does new run
+
+cacheSolve <- function(x, ...) {      #  if
+    m <- x$getSolve()                 #  already ran
+    if(!is.null(m)) {                 #  message
+      message("getting cached data")  #  getting cached data
+      return(m)                       #  return cache
+    }
+  
+    data <- x$get()                   #  if no run
+    m <- solve(data, ...)             #  solve inverse 
+    x$setSolve(m)                     #  of matrix
+    m                                 #  output that
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## mtx <- matrix(rexp(100), 10)    ##      example          ##        for 
+## mtx2 <- makeCacheMatrix(mtx)    ##      of how to        ##      future
+#  cacheSolve(mtx2)                ##         use           ##     reference
